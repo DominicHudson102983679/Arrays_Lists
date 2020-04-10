c7b7f6a47e750a739bae539e5d43f04f88f61e4b
DominicHudson102983679 <61397178+DominicHudson102983679@users.noreply.github.com>
2020-04-10 10:11:09 +1000

    tasks

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Arrays_Lists
 {
@@ -6,7 +8,79 @@ namespace Arrays_Lists
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // --------------------------------------------------------------------Arrays 1 system writes hello 5 times without name
+
+            
+            /*
+            
+            string[] names = new string[5];
+
+            names[1] = "Harry";
+            names[3] = "Jack";
+            names[0] = "Nathan";
+            names[4] = "Lachlan";
+            names[2] = "Bob";
+
+
+
+            for (int i = 0; i < names.Length;)
+            {
+                Console.WriteLine("Hello "); names[0];
+                i++;
+            }
+
+            Console.ReadLine();
+
+            
+            */
+
+            //-------------------------------------------------------------------Arrays 2 COMPLETED un-comment using System.Linq; for addition in arrays
+
+
+            /*
+            
+            int[] numbers = { 34, 5, 67, 1, 99, 34, 44, 78, 34, 0};
+
+            Console.WriteLine(numbers.Sum());
+
+            */
+
+
+            //------------------------------------------------------------------Lists 1 stuck on bool and list.contains
+
+
+            /*
+
+
+            List numbers = new List<int>() { 1, 3, 6, 7, 9 };
+
+            Console.WriteLine("Enter a number less than 10:");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            if (input = List.Contains(numbers))
+            {
+                Console.WriteLine("This number is already on the list");
+            }
+
+            Console.WriteLine("This number is not on the list");
+
+            
+            */
+
+
+            //------------------------------------------------------------------Lists 2
+
+            
+            /*
+            
+            var numbers = new List<int>() { 1, 1, 2, 3, 3, 3, 3, 4, 5, 5};
+
+            Console.WriteLine("Enter a number between 1 and 5:");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+
+            */
+            //dont know what syntax to look for now to find the # of values in {}
         }
     }
 }
